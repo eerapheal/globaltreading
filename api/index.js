b7404c5373894ca4c5ef16cab8be311113ddb123
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-02-05 22:26:12 -0800

    Database connention with mongoose

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Database is connented");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is on 3000')
-})
+  console.log("Server is on 3000");
+});
