f6905b08a3e6816675e9571febf906e49650ee51
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-02-06 03:17:44 -0800

    Middleware error handler

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.rout.js";
 
 dotenv.config();
 
+// MongoDB connection
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => {
@@ -17,9 +18,26 @@ mongoose
 
 const app = express();
 
+// use json
 app.use(express.json());
+
+// Routes
+
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+// Middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
+// Localhost server connection
 
 app.listen(3000, () => {
   console.log("Server is on 3000");
