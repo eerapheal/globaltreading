69ec7379388a4c7f534777e92fe59bc7c2ba8620
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-02-07 07:46:16 -0800

    Tab url set with search params

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import Profile from "../component/Profile";
+import DashboardSidebar from "../component/DashboardSidebar";
+
 const Dashboard = () => {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
   return (
-    <>
-      <h1 className="text-3xl text-red-500">Dashboard</h1>
-    </>
+    <div>
+      <div>
+        <DashboardSidebar />
+      </div>
+      <div>{tab === "profile" && <Profile />}</div>
+    </div>
   );
 };
 
