33e12d18cd8af611689efb78aa28301ccf96a272
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-02-06 20:16:51 -0800

    Login api

--- api/controllers/auth.contoller.js
+++ api/controllers/auth.contoller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs';
-import errorHandler from '../utils/error.js';
+import bcryptjs from "bcryptjs";
+import errorHandler from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -13,7 +14,7 @@ export const signup = async (req, res, next) => {
     email === "" ||
     password === ""
   ) {
-    next(errorHandler(400, "All field is requred" ));
+    next(errorHandler(400, "All field is requred"));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -27,6 +28,45 @@ export const signup = async (req, res, next) => {
     await newUser.save();
     res.json("Signup successful");
   } catch (error) {
-    next(error)
+    next(error);
+  }
+};
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign(
+      {
+        id: validUser._id,
+      },
+      process.env.JWT_SECRET
+    );  
+
+    const {password: pass, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
