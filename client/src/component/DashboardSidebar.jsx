77e188d6c5c99954768132a31fc2541570ea1930
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-02-08 13:09:31 +0530

    a tag bug fix

--- client/src/component/DashboardSidebar.jsx
+++ client/src/component/DashboardSidebar.jsx
@@ -24,6 +24,7 @@ const DashboardSidebar = () => {
               icon={HiUser}
               label={"user"}
               labelColor="dark"
+              as='div'
             >
               Profile
             </Sidebar.Item>
