b55a8629551883c40f945d103d80d7bb5037748d
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-02-10 22:20:56 +0530

    signout user api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -65,3 +65,11 @@ export const deleteUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signout = (req, res, next) => {
+  try {
+    res.clearCookie("access_token").status(200).json("User has been signout");
+  } catch (error) {
+    next(error);
+  }
+};
