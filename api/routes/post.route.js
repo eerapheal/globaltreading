12c47100b2f80e5dc77e4ce753883eb0912accc1
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-02-15 15:27:16 +0530

    update post api

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { create, getPost, deletepost } from "../controllers/post.controller.js";
+import {
+  create,
+  getPost,
+  deletepost,
+  updatepost,
+} from "../controllers/post.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -7,4 +12,6 @@ const router = express.Router();
 router.post("/create", verifyToken, create);
 router.get("/getPost", getPost);
 router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
+router.put("/updatepost/:postId/:userId", verifyToken, updatepost);
+
 export default router;
