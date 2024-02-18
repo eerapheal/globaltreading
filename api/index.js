0dd4452356b3bc3b46888ed70f611c4a64ae26bb
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-02-18 13:14:07 +0530

    comment likes api

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
+import likeCommentRoutes from "./routes/likeComment.route.js";
 
 dotenv.config();
 
@@ -31,6 +32,7 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
+app.use("/api/likeComment", likeCommentRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
