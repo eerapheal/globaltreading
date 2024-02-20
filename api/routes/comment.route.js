c4ff2813a6f614e762b8ccf6913e934b0bc2dba9
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-02-20 23:42:28 +0530

    delete comment api

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { comment, getPostComment, updateComment } from "../controllers/comment.controller.js";
+import { comment, getPostComment, updateComment, deleteComment} from "../controllers/comment.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/comment", verifyToken, comment);
 router.get("/getPostComment/:postId", getPostComment);
 router.put("/updateComment/:commentId", verifyToken, updateComment);
+router.put("/deleteComment/:commentId", verifyToken, deleteComment);
 
 export default router;
