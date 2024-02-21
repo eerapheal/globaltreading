fabd0e6d5b88ab9b7b34e84ff400104c04453623
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-02-21 22:54:22 +0530

    get all comment api

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { comment, getPostComment, updateComment, deleteComment} from "../controllers/comment.controller.js";
+import {
+  comment,
+  getPostComment,
+  updateComment,
+  deleteComment,
+  getComment,
+} from "../controllers/comment.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/comment", verifyToken, comment);
+router.get("/getComment", verifyToken, getComment);
 router.get("/getPostComment/:postId", getPostComment);
 router.put("/updateComment/:commentId", verifyToken, updateComment);
 router.delete("/deleteComment/:commentId", verifyToken, deleteComment);
