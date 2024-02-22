deb1cbe972f1a3453084cfe33c0f86b39625a908
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-02-23 04:59:26 +0530

    deployment setup for render

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
 import likeCommentRoutes from "./routes/likeComment.route.js";
+import path from "path";
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
 const app = express();
 
 app.use(express.json());
@@ -33,6 +35,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 app.use("/api/likeComment", likeCommentRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
